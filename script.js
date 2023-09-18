10b24ae909874fd69d40195e03d8ce3bdd180016
Felopateer1 <137506051+Felopateer1@users.noreply.github.com>
2023-09-18 16:34:57 +0300

    simpa

---
+++ script.js
@@ -0,0 +1,32 @@
+function alertwhendownload() {
+  let title = document.getElementById("title");
+  let textgameforalert = document.getElementById("textgameforalert");
+  let alert = document.getElementById("alert");
+  alert.style.display = "block";
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+  alert.innerHTML = `Downloading...`;
+  if (navigator.onLine == false) {
+    window.close();
+  }
+}
+// search bar
+function search_animal() {
+  let input = document.getElementById("searchbar").value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName("game");
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.cssText = `
+        display: flex;
+  background-color: rgba(0, 0, 0, 0.2);
+  padding: 20px;
+  flex: 50%;
+        `;
+    }
+  }
+}
